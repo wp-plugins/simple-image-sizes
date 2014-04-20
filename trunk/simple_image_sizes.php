4853ab0b3634952fb2a9ac465b85552897268093
Rahe <Rahe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 14:18:58 +0000

    Version 3.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-image-sizes@898719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/simple_image_sizes.php
+++ trunk/simple_image_sizes.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: Simple Image Sizes
-Plugin URI: https://github.com/Rahe/Simple-image-sizes
+Plugin URI: https://github.com/Rahe/'simple-image-sizes'
 Description: Add options in media setting page for images sizes
-Version: 2.4.3
+Version: 3.0
 Author: Rahe
 Author URI: http://nicolas-juen.fr
-Text Domain: sis
-Domain Path: /languages/
+Text Domain: 'simple-image-sizes'
+Domain Path: /languages
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -28,22 +28,33 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-define( 'SIS_URL', plugins_url('', __FILE__) );
-define( 'SIS_DIR', dirname(__FILE__) );
-define( 'SIS_VERSION', '2.4.3' );
+define( 'SIS_URL', plugin_dir_url ( __FILE__ ) );
+define( 'SIS_DIR', plugin_dir_path( __FILE__ ) );
+define( 'SIS_VERSION', '3.0' );
 define( 'SIS_OPTION', 'custom_image_sizes' );
 
-require_once( SIS_DIR . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'class.admin.php'  );
-require_once( SIS_DIR . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'class.client.php'  );
+// Function for easy load files
+function _sis_load_files($dir, $files, $prefix = '') {
+	foreach ( $files as $file ) {
+		if ( is_file( $dir . $prefix . $file . ".php" ) ) {
+			require_once( $dir . $prefix . $file . ".php" );
+		}
+	}	
+}
+
+// Plugin client classes
+_sis_load_files( SIS_DIR . 'classes/', array( 'main' ) );
+
+// Admins classes
+_sis_load_files( SIS_DIR . 'classes/admin/', array( 'main', 'post', 'media' ) );
 
 add_action ( 'plugins_loaded', 'initSIS' );
 function initSIS() {
-	global $SIS;
 	if( is_admin() ) {
-		$SIS['admin'] = new SISAdmin();
+		new SIS_Admin_Main();
+		new SIS_Admin_Post();
+		new SIS_Admin_Media();
 	}
 	
-	$SIS['client'] = new SISClient();
-	
-	load_plugin_textdomain ( 'sis', false, basename( rtrim( SIS_DIR, '/' ) ) . '/languages' );
+	new SIS_Client();
 } 
--- trunk/templates/options-media.php
+++ trunk/templates/options-media.php
@@ -7,15 +7,15 @@
 		<input type="hidden" class="getList" value='<?php echo wp_create_nonce( 'getList' ); ?>' />
 		<div id="sis-regen">
 			<div class="wrapper" style="">
-				<h4> <?php _e( 'Select which thumbnails you want to rebuild:', 'sis'); ?> </h4>
+				<h4> <?php _e( 'Select which thumbnails you want to rebuild:', 'simple-image-sizes'); ?> </h4>
 				<table cellspacing="0" id="sis_sizes" class="widefat page fixed sis">
 					<thead>
 						<tr>
 							<th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input checked="checked" type="checkbox"></th>
-							<th class="manage-column" scope="col"><?php _e( 'Size name', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Width', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Height', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Crop ?', 'sis'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Size name', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Width', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Height', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Crop ?', 'simple-image-sizes'); ?></th>
 						</tr>
 					</thead>
 					<tbody>
@@ -34,7 +34,12 @@
 							$height = isset( $_wp_additional_image_sizes[$s]['height'] ) ? intval( $_wp_additional_image_sizes[$s]['height'] ) : get_option( "{$s}_size_h" ) ;
 							
 							//Set crop
-							$crop = isset( $_wp_additional_image_sizes[$s]['crop'] ) ? intval( $_wp_additional_image_sizes[$s]['crop'] ) : get_option( "{$s}_crop" ) ;
+							$crop = isset( $_wp_additional_image_sizes[$s]['crop'] ) ? $_wp_additional_image_sizes[$s]['crop'] : get_option( "{$s}_crop" ) ;
+							if( is_numeric( $crop ) || is_bool( $crop ) ) {
+								$crop = absint( $crop ) > 0 ? __( 'Yes', 'simple-image-sizes' ) : __( 'No', 'simple-image-sizes' ) ;
+							} else {
+								$crop = Sis_Admin_Main::get_crop_position_label( $crop );
+							}
 							
 							?>
 							<tr id="sis-<?php echo esc_attr( $s ) ?>">
@@ -45,13 +50,13 @@
 									<label for="<?php esc_attr_e( $s ); ?>"><?php echo esc_html( $s ); ?></label>
 								</th>
 								<th>
-									<label for="<?php esc_attr_e( $s ); ?>"><?php echo esc_html( $width); ?> px</label>
+									<label for="<?php esc_attr_e( $s ); ?>"><?php echo esc_html( $width ); ?> px</label>
 								</th>
 								<th>
 									<label for="<?php esc_attr_e( $s ); ?>"><?php echo esc_html( $height ); ?> px</label>
 								</th>
 								<th>
-									<label for="<?php esc_attr_e( $s ); ?>"><?php echo ( $crop == 1 )? __( 'Yes', 'sis' ):__( 'No', 'sis' ); ?>	</label>
+									<label for="<?php esc_attr_e( $s ); ?>"><?php echo $crop; ?> </label>
 								</th>
 							</tr>
 						<?php endforeach;?>
@@ -59,26 +64,26 @@
 					<tfoot>
 						<tr>
 							<th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input checked="checked" type="checkbox"></th>
-							<th class="manage-column" scope="col"><?php _e( 'Size name', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Width', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Height', 'sis'); ?></th>
-							<th class="manage-column" scope="col"><?php _e( 'Crop ?', 'sis'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Size name', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Width', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Height', 'simple-image-sizes'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Crop ?', 'simple-image-sizes'); ?></th>
 						</tr>
 					</tfoot>
 				</table>
 				
-				<h4><?php _e( 'Select which post type source thumbnails you want to rebuild:', 'sis'); ?></h4>
+				<h4><?php _e( 'Select which post type source thumbnails you want to rebuild:', 'simple-image-sizes'); ?></h4>
 				<table cellspacing="0" class="widefat page fixed sis">
 						<thead>
 							<tr>
 								<th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input checked="checked" type="checkbox"></th>
-								<th class="manage-column" scope="col"><?php _e( 'Post type', 'sis'); ?></th>
+								<th class="manage-column" scope="col"><?php _e( 'Post type', 'simple-image-sizes'); ?></th>
 							</tr>
 						</thead>
 						<tbody>
 						<?php
 						// Diplay the post types table
-						foreach ( get_post_types( array( 'public' => true, '_builtin' => false ), 'objects', 'or' ) as $ptype ):
+						foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $ptype ):
 							// Avoid the post_types without post thumbnails feature
 							if( !array_key_exists( 'thumbnail' , $_wp_post_type_features[$ptype->name] ) || $_wp_post_type_features[$ptype->name] == false ) {
 								continue;
@@ -99,7 +104,7 @@
 					<tfoot>
 						<tr>
 							<th scope="col" id="cb" class="manage-column column-cb check-column"><input checked="checked" type="checkbox"></th>
-							<th class="manage-column" scope="col"><?php _e( 'Post type', 'sis'); ?></th>
+							<th class="manage-column" scope="col"><?php _e( 'Post type', 'simple-image-sizes'); ?></th>
 						</tr>
 					</tfoot>
 				</table>
@@ -107,13 +112,18 @@
 		</div>
 		<div >
 			<div id="regenerate_message"></div>
-			<div class="progress"></div>
-			<div id="sis_progress-percent" class="hidden" >0%</div>
+			<div class="media-item sis">
+				<div class="progress">
+					<div id="sis_progress-percent" class="percent">100%</div>
+					<div class="bar"></div>
+				</div>
+			</div>
+			
 			<div class="ui-widget" id="time">
 				<div class="ui-state-highlight ui-corner-all"> 
 					<p>
 						<span class="ui-icon ui-icon-info"></span> 
-						<span><strong><?php _e( 'End time calculated :', 'sis' ); ?></strong> <span class='time_message'><?php _e( 'Calculating...', 'sis' ) ?></span> </span>
+						<span><strong><?php _e( 'End time calculated :', 'simple-image-sizes' ); ?></strong> <span class='time_message'><?php _e( 'Calculating...', 'simple-image-sizes' ) ?></span> </span>
 					</p>
 					<ul class="messages"></ul>
 				</div>
@@ -124,6 +134,6 @@
 					</ol>
 				</p>
 			</div>
-			<div id="thumb"><h4><?php _e( 'Last image:', 'sis'); ?></h4><img id="thumb-img" /></div>
-			<input type="button" class="button" name="ajax_thumbnail_rebuild" id="ajax_thumbnail_rebuild" value="<?php _e( 'Regenerate Thumbnails', 'sis' ) ?>" />
+			<div id="thumb"><h4><?php _e( 'Last image:', 'simple-image-sizes'); ?></h4><img id="thumb-img" /></div>
+			<input type="button" class="button" name="ajax_thumbnail_rebuild" id="ajax_thumbnail_rebuild" value="<?php _e( 'Regenerate Thumbnails', 'simple-image-sizes' ) ?>" />
 		</div>
